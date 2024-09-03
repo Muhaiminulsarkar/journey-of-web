08e5c8371af2126ba0980302119e824af9cdd49e
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-09-03 20:36:32 +0600

    object updated

---
+++ All Milestone/Milestone-4/Module-20(string_object)/declare.js
@@ -0,0 +1,15 @@
+const pen = { brand: 'econo', price: 10, color: 'black' }
+const pencil = new Object()
+console.log(pencil)
+const rubber = Object.create({})
+console.log(rubber)
+
+// using new 
+
+
+let str = "Mississippi";
+console.log(str.indexOf("i", 3));
+
+const str = "Hello";
+str[0] = "h";
+console.log(str);
