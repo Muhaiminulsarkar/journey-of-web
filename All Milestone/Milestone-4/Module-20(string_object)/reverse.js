24df811c09dfb9a9c0a04988254c1c655b7c6a13
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-09-05 22:51:31 +0600

    updated

--- All Milestone/Milestone-4/Module-20(string_object)/reverse.js
+++ All Milestone/Milestone-4/Module-20(string_object)/reverse.js
@@ -26,7 +26,7 @@ for (let i = 0; i < sentence.length; i++) {
     const letter = sentence[i];
     rev = letter + rev;
 }
-console.log(rev);
+// console.log(rev);
 
 // shortcut
 const reversed = sentence.split('').reverse().join('');
