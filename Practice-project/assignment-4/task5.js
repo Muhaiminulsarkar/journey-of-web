b1f0ad0bf7c33bf2833f0cf6acb8a695ceedf90c
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-09-12 21:04:57 +0600

    task 5 added

--- Practice-project/assignment-4/task5.js
+++ Practice-project/assignment-4/task5.js
@@ -1,13 +1,32 @@
 function waitingTime(waitingTimes, serialNumber) {
-    console.log(waitingTimes, serialNumber);
+    console.log(waitingTimes.length, serialNumber);
 
-    if ((Array.isArray(waitingTimes) !== true) || (typeof serialNumber !== 'number')) {
+    if ((Array.isArray(waitingTimes) !== true) || (typeof serialNumber !== 'number'))  {
         const sms = "Invalid Input";
         return sms;
     }
 
+    const length = waitingTimes.length;
+    let sum = 0;
+    for (let time of waitingTimes) {
+        // console.log(time);
+        sum = sum + time;
+    }
+    // console.log(sum);
+    const avg = sum / length;
+    const round = Math.round(avg);
+    // console.log(round)
+
+    const previousPerson = (serialNumber - 1);
+
+    // console.log(previousPerson);
+    const remainPerson = previousPerson - length;
+    const result = remainPerson * round;
+    // console.log(result);
+    return result;
+ 
 }
 
-const arr = [3, 5, 7, 11, 6];
-const result = waitingTime(arr, 10);
+const arr = [7, 8, 3, 4, 5];
+const result = waitingTime(arr, 7);
 console.log(result);
