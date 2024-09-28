ad9056bd0849346ff4a51ca7df4222dd5afeaa4e
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-09-28 15:17:21 +0600

    map method added

---
+++ All Milestone/Milestone-6/Module-30(es6)/more-map.js
@@ -0,0 +1,20 @@
+const num = [2, 5, 6, 5, 8];
+
+const double = num.map(x => x * 2);
+console.log(double);
+
+
+const numbers = [12, 10, 8, 15, 7];
+
+const doubled = numbers.map(num => num * 2)
+// console.log(doubled)
+const fiveBonus = numbers.map(num => num + 5);
+// console.log(fiveBonus)
+const halves = numbers.map(num => num / 2);
+
+const friends = ['Tom', 'John', 'Micheal', 'Oliver'];
+
+const lengths = friends.map(frnd => frnd.length)
+// console.log(lengths)
+const firstLetter = friends.map(friend => friend[0])
+console.log(firstLetter);
