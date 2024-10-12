3a6e0417c2ebe50404c1902098b8c0235307bd19
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-10-12 18:44:53 +0600

    product showed by localstorage

--- All Milestone/Milestone-6/Module-36(localStorage)/js/cart.js
+++ All Milestone/Milestone-6/Module-36(localStorage)/js/cart.js
@@ -12,13 +12,45 @@ const addProduct = () => {
     console.log(product, quantity);
 
     displayProduct(product, quantity);
+    saveProductToLocalStorage(product, quantity);
+
 }
 
 const displayProduct = (product, quantity) => {
     const li = document.createElement('li');
     li.innerText = `${product} : ${quantity}`
     const container = document.getElementById('product-container');
-
-
     container.append(li);
 }
+
+const getStoredShoppingCart = () => {
+    let cart = {};
+    const storedCart = localStorage.getItem('cart');
+    if (storedCart) {
+        cart = JSON.parse(storedCart);
+    }
+    return cart;
+}
+
+const saveProductToLocalStorage = (product, quantity) => {
+    const cart = getStoredShoppingCart();
+
+    cart[product] = quantity;
+    const cartToString = JSON.stringify(cart);
+    localStorage.setItem('cart', cartToString);
+
+}
+
+const displayProductFromLocalStorage = () => {
+    const savedCart = getStoredShoppingCart();
+    console.log(savedCart);
+    for (let product in savedCart) {
+        const quantity = savedCart[product];
+        console.log(product , quantity);
+
+        displayProduct(product,quantity);
+
+    }
+}
+
+displayProductFromLocalStorage()
