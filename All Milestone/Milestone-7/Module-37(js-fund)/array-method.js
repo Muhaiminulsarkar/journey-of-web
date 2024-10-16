c8bf3fa0ba83494e1863c9763590206f87fb3b6c
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-10-17 01:47:01 +0600

    destructuring added

--- All Milestone/Milestone-7/Module-37(js-fund)/array-method.js
+++ All Milestone/Milestone-7/Module-37(js-fund)/array-method.js
@@ -6,5 +6,28 @@ const products = [
     { name: 'camera', price: 9000, brand: 'canon', color: 'gray' }
 ];
 
+const brands = products.map(product => product.brand);
+// console.log(brands);
 const prices = products.map(product => product.price);
-console.log(prices);
+// console.log(prices);
+
+// products.forEach(product => console.log(product))
+// products.forEach(product => console.log(product.color))
+
+products.forEach(product => {
+
+})
+
+
+// 3. filter
+const cheap = products.filter(product => product.price <= 5000);
+// console.log(cheap);
+
+const specificName = products.filter(p => p.name.includes('n'));
+// console.log(specificName);
+
+
+//4. find
+const special = products.find(p => p.name.includes('n'));
+console.log(special);
+
