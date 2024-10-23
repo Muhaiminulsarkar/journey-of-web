7f7a662d69798c18e52399e021fab427e19506cf
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-10-24 05:02:04 +0600

    show the country data properly and add visited section

--- All Milestone/Milestone-7/Module-40(rest-countries)/react-world-tour/src/components/Countries/Countries.jsx
+++ All Milestone/Milestone-7/Module-40(rest-countries)/react-world-tour/src/components/Countries/Countries.jsx
@@ -1,11 +1,15 @@
 import { useEffect } from "react";
 import { useState } from 'react'
 import Country from "../Country/Country";
+
 import './Countries.css';
+
 
 const Countries = () => {
 
     const [countries, setCountries] = useState([])
+
+    const [visitedCountries, setVisitedCountries] = useState([])
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
@@ -13,14 +17,33 @@ const Countries = () => {
             .then(data => setCountries(data))
     }, [])
 
+    const handleVisitedCountry = country => {
+
+        const countryList = [...visitedCountries, country]
+        setVisitedCountries(countryList);
+    }
+    console.log(visitedCountries);
+
     return (
         <div >
             <h3>Countries: {countries.length}</h3>
+
+            <div>
+                <h4>Visited Countries:{visitedCountries.length}</h4>
+                <ul>
+                    {
+                        visitedCountries.map(country => <li
+                            key={country.cca3}>{country.name.common}</li>)
+                    }
+
+                </ul>
+            </div>
+
             <div className="country-container">
 
-            {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
-            }
+                {
+                    countries.map(country => <Country handleVisitedCountry={handleVisitedCountry} key={country.cca3} country={country}></Country>)
+                }
             </div>
         </div>
     );
--- All Milestone/Milestone-7/Module-40(rest-countries)/react-world-tour/src/components/Country/Country.jsx
+++ All Milestone/Milestone-7/Module-40(rest-countries)/react-world-tour/src/components/Country/Country.jsx
@@ -1,13 +1,24 @@
 
+import { useState } from 'react'
 import './Country.css'
 
-export default function Country({ country }) {
-    const { flags, name } = country
+export default function Country({ country, handleVisitedCountry }) {
+    const { flags, name, cca3 } = country
+
+    const [visited, setVisited] = useState(false)
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
 
     return (
-        <div className='country'>
-            <p>Country Name: {name.common}</p>
+        <div className={`country ${visited && 'visited'}`}>
+            <p style={{ color: visited ? 'blue' : 'black' }}>Country: {name.common}</p>
             <img src={flags.png} alt="" />
+            <p><small>Code:{cca3}</small></p>
+            <button onClick={() => handleVisitedCountry(country)}>Mark Visited</button> <br />
+            <button onClick={handleVisited}>{visited ? "Visited" : "Visit"}</button> <br />
+            {visited ? "I have visited" : "I want to go"}
         </div>
     )
 }
