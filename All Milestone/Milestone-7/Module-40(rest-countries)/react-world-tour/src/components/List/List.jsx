c3c692ab677d37fab02d291280214418f66bfbb7
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-10-24 22:23:22 +0600

    component adding

---
+++ All Milestone/Milestone-7/Module-40(rest-countries)/react-world-tour/src/components/List/List.jsx
@@ -0,0 +1,11 @@
+
+
+const List = () => {
+    return (
+        <div className='h:10px'>
+            <img className='h-full w-full obeject-cover' src="" alt="" />
+        </div>
+    );
+};
+
+export default List;
