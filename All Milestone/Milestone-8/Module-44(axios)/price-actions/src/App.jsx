8910d8f4b12b2f4aefcad630f03f32453d350f9a
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-10-29 02:12:33 +0600

    price action added

--- All Milestone/Milestone-8/Module-44(axios)/price-actions/src/App.jsx
+++ All Milestone/Milestone-8/Module-44(axios)/price-actions/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import NavBar from './components/Navbar/NavBar'
+import PriceOptions from './components/PriceOptions/PriceOptions'
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
 
       <h1 className='text-4xl'>Vite + React</h1>
       <NavBar></NavBar>
-
+      <PriceOptions></PriceOptions>
     </>
   )
 }
