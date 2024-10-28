fe38f11bc7dc67ae4e4a04c64795d64af7097adf
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-10-29 01:56:38 +0600

    responsive navbar created

--- All Milestone/Milestone-8/Module-44(axios)/price-actions/src/components/Navbar/NavBar.jsx
+++ All Milestone/Milestone-8/Module-44(axios)/price-actions/src/components/Navbar/NavBar.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import Link from "../Link/Link";
+import { RiMenuFill } from 'react-icons/ri';
+import { RxCross2 } from "react-icons/rx";
 
 
 export default function NavBar() {
+    const [open, setOpen] = useState(false)
 
     const routes = [
         { id: 1, path: '/', name: 'Home' },
@@ -13,7 +17,15 @@ export default function NavBar() {
 
     return (
         <nav>
-            <ul className="md:flex">
+            <div className="md:hidden" onClick={() => setOpen(!open)} >
+                {
+                    open ? <RxCross2 className="text-3xl " /> : <RiMenuFill className="text-3xl " />
+                }
+                {/* <RiMenuFill className="text-3xl " /> */}
+            </div>
+            <ul className={`md:flex duration-1000 absolute md:static
+            ${open ? 'top-16' : '-top-60'}
+            bg-yellow-200 px-6`}>
                 {
                     routes.map(route => <Link key={route.id} route={route}> </Link>)
                 }
