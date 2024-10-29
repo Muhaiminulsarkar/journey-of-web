bcc8c25d0a0d3e266d29603fb2348976b3399f32
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-10-29 06:19:53 +0600

    adding axios and react components

--- All Milestone/Milestone-8/Module-44(axios)/price-actions/src/App.jsx
+++ All Milestone/Milestone-8/Module-44(axios)/price-actions/src/App.jsx
@@ -1,6 +1,8 @@
 
 import './App.css'
+import LineCharts from './components/LineCharts/LineCharts'
 import NavBar from './components/Navbar/NavBar'
+import Phones from './components/Phones/Phones'
 import PriceOptions from './components/PriceOptions/PriceOptions'
 
 function App() {
@@ -13,6 +15,10 @@ function App() {
       <h1 className='text-4xl'>Vite + React</h1>
       <NavBar></NavBar>
       <PriceOptions></PriceOptions>
+      <br />
+      <LineCharts></LineCharts>
+      <Phones></Phones>
+
     </>
   )
 }
