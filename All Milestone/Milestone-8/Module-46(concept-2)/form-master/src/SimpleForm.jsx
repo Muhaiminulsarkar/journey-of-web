6abc6b48a0bfb03c6ad36973080dcb2052751882
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-10-31 23:32:56 +0600

    form creating

--- All Milestone/Milestone-8/Module-46(concept-2)/form-master/src/SimpleForm.jsx
+++ All Milestone/Milestone-8/Module-46(concept-2)/form-master/src/SimpleForm.jsx
@@ -1,9 +1,23 @@
 
 
 const SimpleForm = () => {
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        console.log(e.target.number.value);
+    }
     return (
         <div>
-            
+            <form onSubmit={handleSubmit}>
+                <input className="input input-bordered mb-2" type="text" placeholder="Name" name="name" />
+                <br />
+                <input className="input input-bordered mb-2" type="email" placeholder="Email" name="email" />
+                <br />
+                <input className="input input-bordered mb-2" type="text" placeholder="Number" name="number" />
+                <br />
+                <input className="btn btn-active btn-neutral" type="Submit" value="Submit" />
+            </form>
         </div>
     );
 };
