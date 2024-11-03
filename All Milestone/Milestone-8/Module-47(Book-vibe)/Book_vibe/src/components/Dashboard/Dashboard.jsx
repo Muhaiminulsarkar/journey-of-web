c2a432092c2edc2f0640bd9188a155eed7900f71
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-11-04 03:21:54 +0600

    book vibe project creating

---
+++ All Milestone/Milestone-8/Module-47(Book-vibe)/Book_vibe/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h2 className="text-5xl">This is a dashboard page</h2>
+        </div>
+    );
+};
+
+export default Dashboard;
