0d65fc721faa7883581416ae18e7299fed9ed280
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-11-19 15:24:20 +0600

    project cloned

---
+++ All Milestone/Milestone-9/Module-51(auth-project!!!!!!!)/auth-project/src/firebase.init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAcUNVtCrZ8LREx59Mvv_LT1YLRkP_i5Js",
+  authDomain: "auth-project-5911a.firebaseapp.com",
+  projectId: "auth-project-5911a",
+  storageBucket: "auth-project-5911a.firebasestorage.app",
+  messagingSenderId: "670508239476",
+  appId: "1:670508239476:web:9cc4175ebc9f074f575f21"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
---
+++ All Milestone/Milestone-9/Module-51(auth-project!!!!!!!)/auth-project/src/main.jsx
@@ -0,0 +1,56 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/Root.jsx';
+import Home from './components/Home.jsx';
+import Login from './components/Login.jsx';
+import Register from './components/Register.jsx';
+
+import Orders from './components/Orders.jsx';
+import PrivateRoute from './routes/PrivateRoute.jsx';
+import Profile from './components/Profile.jsx';
+import AuthProvider from './components/providers/AuthProvider.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      },
+      {
+        path: 'register',
+        element: <Register></Register>
+      },
+      {
+        path: 'orders',
+        element: <PrivateRoute><Orders></Orders></PrivateRoute>
+      },
+      {
+        path: 'profile',
+        element: <PrivateRoute><Profile></Profile></PrivateRoute>
+      }
+    ]
+  },
+]);
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>,
+)
